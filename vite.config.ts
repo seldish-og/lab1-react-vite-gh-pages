e963e363918aa64ec3855c7e33aecba7cdd7fcd1
e.tselishchev <etselishchev@mail.ru>
2025-11-26 14:52:19 +0300

    fix: correct Vitest config types in vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,5 @@
 import react from '@vitejs/plugin-react'
-import { defineConfig } from 'vite'
-import { UserConfig } from 'vitest/config'
+import { defineConfig } from 'vitest/config'
 
 // https://vite.dev/config/
 export default defineConfig({
@@ -11,5 +10,5 @@ export default defineConfig({
     environment: 'jsdom',
     setupFiles: './src/setupTests.ts',
     css: true,
-  } as UserConfig,
+  },
 })
